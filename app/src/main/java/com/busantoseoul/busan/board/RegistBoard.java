afe062fbd5654d540f336111d0e5819a91af179e
nt10260 <garam.kim@nhn.com>
2015-10-31 13:33:29 +0900

    [가람] 텍스트 -> hint 변경 및 대화상자 오류로 해결 시도(진행중)

--- app/src/main/java/com/busantoseoul/busan/board/RegistBoard.java
+++ app/src/main/java/com/busantoseoul/busan/board/RegistBoard.java
@@ -1,8 +1,11 @@
 package com.busantoseoul.busan.board;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.busantoseoul.busan.R;
 
@@ -10,6 +13,8 @@ import com.busantoseoul.busan.R;
  * Created by Naver on 2015-10-25.
  */
 public class RegistBoard extends Activity {
+    private final Context context = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +26,25 @@ public class RegistBoard extends Activity {
     }
 
     public void onClickRegistCancel(View v) {
-        System.out.println("new text cancel good");
+        if (isEmptyContent()) {
+            finish();
+        }
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+
+        dialogBuilder.setTitle("test title");
+        dialogBuilder.setMessage("글 작성을 취소하시겠습니까?");
+        dialogBuilder.setPositiveButton("yes", null);
+        dialogBuilder.setNegativeButton("no", null);
+
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+    }
+
+    private boolean isEmptyContent() {
+        EditText title = (EditText)findViewById(R.id.regist_title);
+        EditText content = (EditText)findViewById(R.id.regist_content);
+
+        return (title.getText() == null || title.getText().toString().equals("")) && (content.getText() == null || content.getText().toString().equals(""));
     }
 }
